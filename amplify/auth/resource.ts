c702e6c1779de833de6c5b1cad4f008722376cfb
ThabisoBlessed <blessedngust13@gmail.com>
2025-12-13 20:12:24 +0200

    first commit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,10 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
+  },  userAttributes: {
+    preferredUsername: {
+      mutable: true,
+      required: true
+    }
+  }
 });
