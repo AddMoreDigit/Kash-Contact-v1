5ce94cdec0ac194e2cdd64e7d5af5ebd1392adf8
ThabisoBlessed <blessedngust13@gmail.com>
2025-12-13 18:41:56 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@services": path.resolve(__dirname, "./src/services"),
+    },
+  },
+   build: {
+      target: 'esnext',
+      outDir: 'dist',
+    },
+    server: {
+      port: 3000,
+      open: true,
+    },
 })
